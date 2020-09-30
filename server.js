f820efe75e17c9b23b8cc20d675eb6139dc8dea9
Dave Baskind <dave@picketstudio.com>
2020-10-01 09:28:38 +1000

    Stub out the first endpoint test

--- server.js
+++ server.js
@@ -3,6 +3,17 @@
 const express = require('express');
 const app = express();
 
+app.post('/api/issues/:projectName', (req, res) => {
+  const testIssue = {
+    issue_title: 'Test issue',
+    issue_text: 'This is a test issue',
+    created_by: 'Mocha',
+    assigned_to: 'Mocha',
+    status_text: 'New'
+  };
+  res.json(testIssue);
+});
+
 app.use('/', (req, res) => {
   res.json({ message: 'Hello, World.' });
 });
--- tests/integration.js
+++ tests/integration.js
@@ -7,17 +7,49 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+const testIssue = {
+  issue_title: 'Test issue',
+  issue_text: 'This is a test issue',
+  created_by: 'Mocha',
+  assigned_to: 'Mocha',
+  status_text: 'New'
+};
+const testIssueKeys = Object.keys(testIssue);
+
 describe('server.js', () => {
   it('says hello', (done) => {
-  chai.request(server)
-  .get('/')
+    chai.request(server)
+      .get('/')
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.have.property('message').that.equals('Hello, World.');
         done();
+      });
   });
-}); 
-  
-})
+  context('When POSTing to /api/issues/{projectname}', () => {
+    let data;
+    before((done) => {
+      chai.request(server)
+        .post('/api/issues/test-project')
+        .type('form')
+        .send(testIssue)
+        .end((err, res) => {
+          if (err) done(err);
+          else {
+            data = res;
+            done();
+          }
+        });
+    });
+    it('accepts form data conforming to the expected type', (done) => {
+      expect(data).to.have.status(200);
+      expect(data).to.be.json;
+      testIssueKeys.forEach((key) => {
+        expect(data.body).to.have.property(key).that.equals(testIssue[key]);
+      });
+      done();
+    });
+  });
+});
